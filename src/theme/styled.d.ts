f0170db43a065fb02ccec339ed021cb7815d3081
Andrew Taranenko <krol2006@bigmir.net>
2022-12-29 13:47:07 +0100

    Update styled.d.ts

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,10 +1,14 @@
 import { TColors, TDevices, TFonts } from "./variables/types";
 
 declare module "styled-components" {
+  interface TRem<P = number> {
+    (px: P): string;
+  }
+
   export interface DefaultTheme {
     colors: TColors;
     devices: TDevices;
     fonts: TFonts;
-    rem: (px: number) => string;
+    rem: TRem;
   }
 }
